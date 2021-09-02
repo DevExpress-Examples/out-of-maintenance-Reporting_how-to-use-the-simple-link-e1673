f6b7c53af0aa5c7800f74ad4b48479c35ade483d
DevExpressExampleBot <fakeEmail>
2021-09-02 20:10:13 +0300

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -8,36 +8,36 @@ Imports DevExpress.Xpf.Printing
 
 Namespace UseCollectionViewLink
 
-    Partial Public Class Window1
-        Inherits Window
+	Partial Public Class Window1
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
 #Region "#SimpleLink"
 Private data() As String
 
 Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-    ' Create an array of strings.
-    data = CultureInfo.CurrentCulture.DateTimeFormat.DayNames
+	' Create an array of strings.
+	data = CultureInfo.CurrentCulture.DateTimeFormat.DayNames
 
-    ' Create a link and specify a template and detail count for it.
-    Dim link As New SimpleLink()
-    link.DetailTemplate = DirectCast(Resources("dayNameTemplate"), DataTemplate)
-    link.DetailCount = data.Length
+	' Create a link and specify a template and detail count for it.
+	Dim link As New SimpleLink()
+	link.DetailTemplate = DirectCast(Resources("dayNameTemplate"), DataTemplate)
+	link.DetailCount = data.Length
 
-    ' Create a document.
-    AddHandler link.CreateDetail, AddressOf link_CreateDetail
+	' Create a document.
+	AddHandler link.CreateDetail, AddressOf link_CreateDetail
 
-    ' Show a Print Preview window.
-    PrintHelper.ShowPrintPreviewDialog(Me, link)
+	' Show a Print Preview window.
+	PrintHelper.ShowPrintPreviewDialog(Me, link)
 End Sub
 
 Private Sub link_CreateDetail(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
-    e.Data = data(e.DetailIndex)
+	e.Data = data(e.DetailIndex)
 End Sub
 #End Region ' #SimpleLink
-    End Class
+	End Class
 
 End Namespace
