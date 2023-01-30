800b5f65c481ae1b0d66e29ee5cff954e04ca06f
DevExpressExampleBot <fakeEmail>
2023-01-30 15:38:04 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,43 +1,39 @@
-﻿#Region "#Reference"
+'#Region "#Reference"
 Imports System
 Imports System.Globalization
 Imports System.Windows
 Imports DevExpress.Xpf.Printing
-' ...
-#End Region ' #Reference
 
+' ...
+'#End Region  ' #Reference
 Namespace UseCollectionViewLink
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-#Region "#SimpleLink"
-Private data() As String
+'#Region "#SimpleLink"
+        Private data As String()
 
-Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-	' Create an array of strings.
-	data = CultureInfo.CurrentCulture.DateTimeFormat.DayNames
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ' Create an array of strings.
+            data = CultureInfo.CurrentCulture.DateTimeFormat.DayNames
+            ' Create a link and specify a template and detail count for it.
+            Dim link As SimpleLink = New SimpleLink()
+            link.DetailTemplate = CType(Resources("dayNameTemplate"), DataTemplate)
+            link.DetailCount = data.Length
+            ' Create a document.
+            AddHandler link.CreateDetail, New EventHandler(Of CreateAreaEventArgs)(AddressOf link_CreateDetail)
+            ' Show a Print Preview window.
+            PrintHelper.ShowPrintPreviewDialog(Me, link)
+        End Sub
 
-	' Create a link and specify a template and detail count for it.
-	Dim link As New SimpleLink()
-	link.DetailTemplate = DirectCast(Resources("dayNameTemplate"), DataTemplate)
-	link.DetailCount = data.Length
-
-	' Create a document.
-	AddHandler link.CreateDetail, AddressOf link_CreateDetail
-
-	' Show a Print Preview window.
-	PrintHelper.ShowPrintPreviewDialog(Me, link)
-End Sub
-
-Private Sub link_CreateDetail(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
-	e.Data = data(e.DetailIndex)
-End Sub
-#End Region ' #SimpleLink
-	End Class
-
+        Private Sub link_CreateDetail(ByVal sender As Object, ByVal e As CreateAreaEventArgs)
+            e.Data = data(e.DetailIndex)
+        End Sub
+'#End Region  ' #SimpleLink
+    End Class
 End Namespace
